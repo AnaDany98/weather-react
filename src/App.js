75315ca95cfdf18d8ed5fa198e61afcb03c6162b
AnaDany98 <105895422+AnaDany98@users.noreply.github.com>
2025-05-13 13:21:31 -0400

    Made some changes to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Weather from "./Weather";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+
         <Weather city="Paris" />
       </header>
     </div>
